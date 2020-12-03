852a24229c5876157bb6da28d26ff0d727a592dc
MdSoikot <MdSoikot>
2020-12-03 21:47:16 +0600

    new commit

--- app/Http/Controllers/corona_update_controller.php
+++ app/Http/Controllers/corona_update_controller.php
@@ -15,7 +15,6 @@ class Corona_update_controller extends Controller
         $death_update = DB::table('corona_update_bans')->where('case','death')->orderBy('created_at', 'DESC')->first();
         $cure_update = DB::table('corona_update_bans')->where('case','cure')->orderBy('created_at', 'DESC')->first();
         $test_update = DB::table('corona_update_bans')->where('case','test')->orderBy('created_at', 'DESC')->first();
-
         return view('users/homepage', compact('infected_update','death_update','cure_update','test_update'));
     }
 
--- resources/views/users/contact.blade.php
+++ resources/views/users/contact.blade.php
@@ -104,33 +104,9 @@
         <!-- end header section -->
 
         <!-- slider section -->
-        <section class="slider_section">
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="https://image.freepik.com/free-psd/coronavirus-prevention-banner-design-template_169869-231.jpg"
-                            class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="https://image.freepik.com/free-psd/virus-prevention-banner-template-with-protect-yourself-against-covid-19-concept_169869-228.jpg"
-                            class="d-block w-100" alt="...">
-
-                    </div>
-
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+        <section class="contact-us">
+            <div class="contact-us-banner" style="">
+            <img src="{{asset("user_template_assets/images/contact-us-banner.jpg")}}" class="" alt="image not found">
             </div>
         </section>
 
